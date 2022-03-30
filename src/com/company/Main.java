c7af0e75a9786c6e51af6ae68ad3bcaa32190ef9
gf211097 <ulyfleen@gmail.com>
2022-03-30 09:34:22 +0100

    removed unnecessary

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // color guessing game
-        // create a random number between 1 and 10
-        // ask user to guess the number
-        // tell user if they are too high or too low
-        // tell user if they are correct
-        // repeat until user is correct
-        // print out the number of guesses it took them
-        //write the code
         int randomNumber = (int) (Math.random() * 10) + 1;
         System.out.println("Guess a number between 1 and 10");
         int guess = getInput("Enter your guess: ");
